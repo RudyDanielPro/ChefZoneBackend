25c6cd1ef9664cda1d2d950fceecafc6a0360fc2
Rudy <rudydanielcarballo@gmail.com>
2026-03-08 14:34:10 -0400

    Ajustando direcciones de fotos

---
+++ src/main/java/ChefZoneBackend/Config/CloudinaryConfig.java
@@ -0,0 +1,30 @@
+package ChefZoneBackend.Config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${Cloud_Name}")
+    private String cloudName;
+
+    @Value("${Cloud_Key}")
+    private String apiKey;
+
+    @Value("${Cloud_Api_Secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret,
+            "secure", true
+        ));
+    }
+}
